c60a1de4c7015659cb99e7a47afc47035a147dbd
karankaruna <sakaran006@gmail.com>
2020-09-15 15:31:12 +0800

    Create OrderConstants.java

---
+++ order-item-service/src/main/java/com/project/order/item/common/OrderConstants.java
@@ -0,0 +1,9 @@
+package com.project.order.item.common;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class OrderConstants {
+
+    public static final String PRODUCT_NAME = "Product Name";
+}
