b6711890434a6d7f9ab72b705c560ce8612f2fec
karankaruna <sakaran006@gmail.com>
2020-09-15 15:34:47 +0800

    Create OrderItemRepository.java

---
+++ order-item-service/src/main/java/com/project/order/item/repository/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package com.project.order.item.repository;
+
+import com.project.order.item.model.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderItemRepository extends JpaRepository<OrderItem,Integer> {
+}
